3eed9ffc1ced456ae9a78d5ff524a9181b671606
Michael Neustadter <mneustadts@outlook.com>
2020-10-19 09:56:12 -0500

    url

--- static/css/js/logic.js
+++ static/css/js/logic.js
@@ -1,2 +1,19 @@
 var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
 var platesURL = "https://github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_plates.json"
+
+// Create color function
+function getColor(magnitude) {
+    if (magnitude > 5) {
+        return 'red'
+    } else if (magnitude > 4) {
+        return 'orange'
+    } else if (magnitude > 3) {
+        return 'yellow'
+    } else if (magnitude > 2) {
+        return 'lightgreen'
+    } else if (magnitude > 1) {
+        return 'green'
+    } else {
+        return 'magenta'
+    }
+};
