37ae3ef8cc65abc261a711c4a4063ac2bdbc8379
Michael Neustadter <mneustadts@outlook.com>
2020-10-19 09:50:17 -0500

    url

---
+++ static/css/js/logic.js
@@ -0,0 +1,2 @@
+var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
+var platesURL = "https://github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_plates.json"
