32d4df1cba19bf1384452981ac8ffe2a390ca0e6
Michael Neustadter <mneustadts@outlook.com>
2020-10-19 15:22:36 -0500

    rework

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,16 +3,16 @@ var myMap = L.map("mapid", {
   zoom: 2
 });
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox.satelite",
+  id: "mapbox/streets-v11",
   accessToken: API_KEY
   }).addTo(myMap)
   
-var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
+var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 //perform api call
 d3.json(quakeURL).then(function(data) {
