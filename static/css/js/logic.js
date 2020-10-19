5d0cef205fb9938978f3c28c3f4042b73dc99c6a
Michael Neustadter <mneustadts@outlook.com>
2020-10-19 10:06:00 -0500

    map

--- static/css/js/logic.js
+++ static/css/js/logic.js
@@ -49,3 +49,27 @@ d3.json(quakeURL, function(data) {
 
     createMap(earthquakes);
 }
+
+function createMap(earthquakes) {
+
+    // Define satelitemap and darkmap layers
+    var satelitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.satellite",
+      accessToken: API_KEY
+    });
+  
+    var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.dark",
+      accessToken: API_KEY
+    });
+  
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+      "Satelite Map": satelitemap,
+      "Dark Map": darkmap
+    };
+  
