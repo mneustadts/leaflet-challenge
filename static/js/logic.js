172f143f086b52b7bf7e176e113f648c890c1994
Michael Neustadter <mneustadts@outlook.com>
2020-10-20 11:20:19 -0500

    rework

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,12 +3,12 @@ var myMap = L.map("mapid", {
   zoom: 2
 });
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox/streets-v11",
+  id: "dark-v10",
   accessToken: API_KEY
   }).addTo(myMap)
   
